b880c2744c817e25c4e64c33a8bfd18483bac3e6
Anand Rawool <anandrawool5624@gmail.com>
2025-01-01 22:58:46 +0530

    update

--- src/Array/easy/Displaylongestname.java
+++ src/Array/easy/Displaylongestname.java
@@ -1,4 +1,15 @@
 package Array.easy;
 
+import java.util.Arrays;
 public class Displaylongestname {
+    public static void main(String[] args) {
+
+        String[] names = {"Geek", "Geeks", "Geeksfor", "GeeksforGeek", "GeeksforGeeks"};
+
+        String longesname =Arrays.stream(names).max((a,b) -> Integer.compare(a.length(),b.length())).
+                orElse("");
+
+        System.out.println(longesname);
+
+    }
 }
