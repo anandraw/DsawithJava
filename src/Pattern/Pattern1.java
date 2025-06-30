0300a2fcd216a393b64c373c9c5e5f712f762240
Anand Rawool <anandrawool5624@gmail.com>
2025-06-30 11:42:35 +0530

    new added

--- src/Pattern/Pattern1.java
+++ src/Pattern/Pattern1.java
@@ -4,7 +4,7 @@ public class Pattern1 {
     public static void main(String[] args) {
         for (int i = 1; i <=4; i++) {
             for (int j = 1; j <=i ; j++) {
-                System.out.print(j+" ");
+                System.out.print("* ");
             }
             System.out.println("");
         }
