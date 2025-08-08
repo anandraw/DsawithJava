86d3797201230860f74bac96b03c79cdee27ca54
Anand Rawool <anandrawool5624@gmail.com>
2025-08-08 16:55:57 +0530

    new added

---
+++ src/Array/medium/LongestPrefixSuffix.java
@@ -0,0 +1,12 @@
+package Array.medium;
+
+public class LongestPrefixSuffix {
+    public static void main(String[] args) {
+        String s = "abab";
+        int ans = getLPSLength(s);
+        System.out.println(ans);
+    }
+     static int getLPSLength(String s) {
+       return 1;
+    }
+}
